042e3b43c38fab3e3214fbfbb8b6662a4964601b
Vincent Olsen <vincent.olsen@outlook.com>
2024-07-13 20:28:18 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <img alt="Bilde av Vincent Olsen" src="./assets/vinco_3000px.jpg"
+  contain
+  height="300px"
+  width="300px">
+  <HelloWorld msg="Vincent sier; hello world"/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
