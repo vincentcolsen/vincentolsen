9712b3ca9e9edc7b2f040ec4ec5910a2b157a69b
Vincent Olsen <vincent.olsen@outlook.com>
2024-08-01 20:09:44 +0200

    added darkmode and lightmode settings

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,23 @@
 <template>
-  <div id="app" class="container">
-      <header></header>
-      <ProfileInfo :profile-info="profileinfo" />
-      <AboutMe :about="about"  />
-      <div class="spacer"></div>
-      <WorkExperience :jobs="jobs" />
-      <WebpageLinks :webpagelinks="webpagelinks" />
+  <div :class="{'background-dark': !isToggled, 'background-light': isToggled}">
+    <header class="app-header">
+      <div class="header-content">
+        <div class="funmodediv">
+          <label class="switch">
+            <input type="checkbox" v-model="isToggled" @change="toggleMode">
+            <span class="slider round"></span>
+          </label>
+        </div>
+      </div>
+    </header>
+    <div id="app" class="container" >
+        <ProfileInfo :profile-info="profileinfo" />
+        <AboutMe :about="about"  />
+        <div class="spacer"></div>
+        <WorkExperience :jobs="jobs" />
+        <WebpageLinks :webpagelinks="webpagelinks" />
+    </div>
+    <FunMode/>
   </div>
 </template>
 
@@ -14,6 +26,7 @@ import ProfileInfo from "@/components/ProfileInfo.vue";
 import AboutMe from "@/components/AboutMe.vue";
 import WorkExperience from "@/components/WorkExperience.vue";
 import WebpageLinks from "@/components/WebpageLinks.vue";
+import FunMode from "@/components/FunMode.vue";
 
 export default {
   name: 'App',
@@ -21,10 +34,12 @@ export default {
     ProfileInfo,
     AboutMe,
     WorkExperience,
-    WebpageLinks
+    WebpageLinks,
+    FunMode
   },
   data() {
     return {
+      isToggled: false,
       profileinfo: {
           imageSrc: require('@/assets/vinco_3000px.jpg'),
           imageAlt: 'Bilde av Vincent Olsen',
@@ -37,28 +52,28 @@ export default {
       },
       jobs: [
         {
-          id: 1,
+          id: 0,
           duration: '2022 - present',
           title: 'IT Consultant – Netcompany',
           description: 'Currently outsourced as a developer and serving as the security manager for the System Development section at Oslo municipality. Responsible for managing security work, including developing a management system for information security and privacy, and leading the Security Champions expert group. Focused on container orchestration with Kubernetes and OpenShift, leveraging a Certified Kubernetes Administrator (CKA) certification, and utilizing Azure for managing secrets. Additionally, working with Spring Boot and Vue for backend and frontend development.',
           technologies: ['Kubernetes', 'Terraform', 'Ansible', 'Spring Boot', 'Vue', 'Python', 'Java', '...']
         },
         {
-          id: 2,
+          id: 1,
           duration: '2021 - 2021',
           title: 'Summer Internship | Big Data & Analytics – Orkla',
           description: 'In-house consultant focusing on machine learning, analytics, and data integration. Analyzed a large amount of data using Gradient Boosting to determine what drove the sales of Orkla Health and Care.',
           technologies: ['Azure Synapse', 'Azure Databricks', 'Web-APIs', 'Python', 'PowerBI']
         },
         {
-          id: 3,
+          id: 2,
           duration: '2018 - 2022',
           title: 'Service Consultant – Norwegian Information Security Forum',
           description: 'Responsible for the preparation and hosting of member meetings held quarterly and an annual large-scale conference. Assisted the board with various tasks, including supplier and participant coordination, administrative work, order processing, design, and member management.',
           technologies: ['Adobe Illustrator', 'Squarespace']
         },
         {
-          id: 4,
+          id: 3,
           duration: '2017 - 2018',
           title: 'System Consultant – Norconsult',
           description: 'Collaborated with the HR department in rolling out the new Workday HR system across the organization. Actively involved in the implementation and integration of the system.',
@@ -67,18 +82,27 @@ export default {
       ],
       webpagelinks: [
         {
-          id: 1,
+          id: 0,
           title: 'LinkedIn',
           link: 'https://www.linkedin.com/in/vincentolsen/',
           linktext: '@vincentcolsen'
         },
         {
-          id: 2,
+          id: 1,
           title: 'GitHub',
           link: 'https://github.com/vincentcolsen/',
           linktext: '@vincentcolsen'
         }
       ]
+    };
+  },
+  methods: {
+    toggleMode() {
+      if (this.isToggled) {
+        // Do something when toggled on
+      } else {
+        // Do something when toggled off
+      }
     }
   }
 };
@@ -86,11 +110,7 @@ export default {
 </script>
 
 <style>
-
-header {
-  height: 10px;
-}
-
+/* General styles */
 body {
   font-family: Inter, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -103,6 +123,101 @@ body {
   min-height: 100%;
 }
 
+.container {
+  padding: 80px 14px;
+  max-width: 500px;
+  margin: auto;
+}
+
+.spacer {
+  padding-top: 35px;
+}
+
+/* Header styles */
+.app-header {
+  display: flex;
+  justify-content: flex-end;
+  padding: 10px 20px;
+}
+
+.funmodediv {
+  position: relative;
+}
+
+/* Dark mode styles */
+.background-dark {
+  background-color: #121212;
+  color: #FFFFFF;
+}
+
+/* Light mode styles */
+.background-light {
+  background-color: #F5F5F5;
+  color: #000000;
+}
+
+.background-light p {
+  color: #121212;
+}
+
+.background-light .technologies li {
+  font-weight: normal;
+}
+
+/* Toggle switch stykes */
+.switch {
+  position: relative;
+  display: inline-block;
+  width: 34px;
+  height: 20px;
+}
+
+.switch input {
+  opacity: 0;
+  width: 0;
+  height: 0;
+}
+
+.slider {
+  position: absolute;
+  cursor: pointer;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background-color: #F5F5F5;
+  transition: .4s;
+}
+
+.slider:before {
+  position: absolute;
+  content: "";
+  height: 14px;
+  width: 14px;
+  left: 3px;
+  bottom: 3px;
+  background-color: #121212;
+  transition: .4s;
+}
+
+input:checked + .slider {
+  background-color: #121212;;
+}
+
+input:checked + .slider:before {
+  transform: translateX(14px);
+  background-color: #F5F5F5;
+}
+
+.slider.round {
+  border-radius: 34px;
+}
+
+.slider.round:before {
+  border-radius: 50%;
+}
+
+/* Text styles */
 h1 {
   font-size: 22px;
   margin: 10px 0;
@@ -127,25 +242,4 @@ p {
   line-height: 21px;
   color: rgba(255, 255, 255, 0.64);
 }
-
-.container {
-  padding: 115px 14px;
-  -webkit-padding-start: 1rem;
-  padding-inline-start: 1rem;
-  -webkit-padding-end: 1rem;
-  padding-inline-end: 1rem;
-  max-width: 500px;
-  -webkit-margin-start: auto;
-  margin-inline-start: auto;
-  -webkit-margin-end: auto;
-  margin-inline-end: auto;
-}
-
-
-
-.spacer {
-  padding-top: 35px;
-}
-
-
 </style>
